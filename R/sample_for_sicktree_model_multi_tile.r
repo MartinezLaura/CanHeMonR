4adeb468a1019da39f7938772e52993de01fe7a0
Laura Martinez Sanchez <martlur@d01ri1301208.ies.jrc.it>
2017-08-07 14:28:13 +0200

    172/5000 The conditional that verifies that the raster and the shapefile have the same projection has been moved so that this check is made before the crop of the points. Added a parser that avoids the duplication of names if the textures and the tiles are on the same folder. Ex: pt43-34 and pt43_34 and to ignore the files ended with "aux.xml" that QGis create
    Added a coditional to go to the next tile if the previous one doesn't intersect qith any vuln_classes

--- R/sample_for_sicktree_model_multi_tile.r
+++ R/sample_for_sicktree_model_multi_tile.r
@@ -22,10 +22,10 @@
 #' class_test_path <- '//ies.jrc.it/h03/FISE/forest/CanopyHealthMonitoring/PWN/classification_tests'
 #' training_pol_filename <- file.path(class_test_path,'cal_val_data/Castelo_Branco_DMC_Nov2016/DMC_Nov2016_inspect_multi_final_20170126.shp')
 #' Pols <- raster::shapefile(training_pol_filename)
-# tt <-  sample_for_sicktree_model_multi_tile(r_train_dir <-'/media/laura/Laura/ADS100_06032017/ADS_sample/', tile = 'ALL', vuln_classes <- list(c('Pb')),
-#                                             training_pol_filename <- '/media/laura/Laura/visual_interpretation/visual_interpretation_ADS/ADS100_Aug2015_inspect_20170313_reproj.shp',
-#                                             field_name = 'type', ninputs_tile = 27, data_outp_dir <- '/media/laura/Laura/Rcode/Sicktree/2-tiles/', abs_samp = 1000,
-#                                             parallel = F, nWorkers = 4, data_outp_name = "maxentprobaLaura")
+# tt <-  sample_for_sicktree_model_multi_tile(r_train_dir <-'/home/martlur/Documents/Dockers/docker6/data_test_docker/', tile = 'ALL', vuln_classes <- list(c('Pb')),
+#                                             training_pol_filename <- '/home/martlur//Documents/Dockers/docker6/visual_interpretation_ADS/ADS100_Aug2015_inspect_20170313_reproj.shp',
+#                                             field_name = 'type', ninputs_tile = 27, data_outp_dir <- '/home/martlur/Documents/Results/Rcode/Testsample/', abs_samp = 10000,
+#                                             parallel = T, nWorkers = 50, data_outp_name = "maxentproba4img")
 #'}
 #' @export
 sample_for_sicktree_model_multi_tile <- function(r_train_dir, tile = 'ALL', vuln_classes, training_pol_filename, field_name, ninputs_tile, data_outp_dir, abs_samp = 1000,
@@ -59,6 +59,8 @@ sample_for_sicktree_model_multi_tile <- function(r_train_dir, tile = 'ALL', vuln
   all_tifs <- all_tifs[grepl('.tif',all_tifs)]
   #excluded the tif files in the unprojected folder
   all_tifs <- all_tifs[!grepl('orig_noPRJ', all_tifs)]
+  #excluded the tif files ending like .aux.xml
+  all_tifs <- all_tifs[!grepl('.aux.xml', all_tifs)]
 
   #if you want to run all the tiles in a directory, harvest the available tilenames
   if (tile[1] == 'ALL'){
@@ -66,9 +68,13 @@ sample_for_sicktree_model_multi_tile <- function(r_train_dir, tile = 'ALL', vuln
     tile <- unique(tile)
     #only keep tiles that start  with 'pt'
     tile <- tile[substr(tile,1,2) == 'pt']
-
+    #only keep the original tiles and avid to duplicate the basename. Ex: pt617000_4404000 and pt617000-4404000
+    #only happends when the textures and the tiles are in the same folder
+    tile <- tile[substr(tile,9,9) == '-']
     cat(length(tile),' tiles are considered\n')
+    print(tile)
   }
+
 
   tile_counter <- 0
   # a list to hold the outputs
@@ -78,11 +84,8 @@ sample_for_sicktree_model_multi_tile <- function(r_train_dir, tile = 'ALL', vuln
   #set up the cluster for parallel processing
   if (parallel){
     try(parallel::stopCluster(cl), silent=T)
-
-
-    # TO DO add a line that avoids allocating more workers than you have cores
-    # set in minimumnot in conditional
-    maxcl <- (parallel::detectCores(logical = FALSE) * parallel::detectCores(logical = TRUE))-1
+    # Test that the cores assign not overlap the maximum cores available
+    maxcl <- (parallel::detectCores(logical = TRUE)-1)
     if (nWorkers <= maxcl){
       cl <- parallel::makeCluster(nWorkers)
       doParallel::registerDoParallel(cl)
@@ -94,134 +97,126 @@ sample_for_sicktree_model_multi_tile <- function(r_train_dir, tile = 'ALL', vuln
   #choose the appropriate operator for the foreach loop
   require(foreach)
   `%op%` <- if (parallel) `%dopar%` else `%do%`
-  length(tile)
-  stime <- system.time({maxent_training_dfs <- foreach::foreach(i = 1:length(tile), .combine = rbind.data.frame, .inorder=F, .multicombine=F, .errorhandling='remove') %op% {
+
+
+  stime <- system.time({maxent_training_dfs <- foreach(i = 1:length(tile), .combine = rbind.data.frame, .inorder=F, .errorhandling='remove') %op% {
     tile_i <- tile[i]
-    print (tile)
-    for (tile_i in tile){
-      #make alternative tile code (Margherita uses these in the texture filenames)
-      tile_i_multiversion <- unique(c(tile_i, gsub('_','-',tile_i),gsub('-','_',tile_i),gsub('-','\\.',tile_i),gsub('_','\\.',tile_i),gsub('\\.','-',tile_i),gsub('\\.','_',tile_i)))
-      tile_i_multiversion_for_regexpr <- paste(tile_i_multiversion, collapse = "|")
-      #pred_rs <- list.files(r_train_dir, recursive = T, full.names = T)
-      #pred_rs <- pred_rs[grepl('.tif',pred_rs)]
-      pred_rs <- all_tifs[grepl(tile_i_multiversion_for_regexpr, all_tifs)]
-      #an empty data frame to hold the data extracted for this tile
-      tile_dat <- data.frame()
+    #make alternative tile code (Margherita uses these in the texture filenames)
+    tile_i_multiversion <- unique(c(tile_i, gsub('_','-',tile_i),gsub('-','_',tile_i),gsub('-','\\.',tile_i),gsub('_','\\.',tile_i),gsub('\\.','-',tile_i),gsub('\\.','_',tile_i)))
+    tile_i_multiversion_for_regexpr <- paste(tile_i_multiversion, collapse = "|")
+    #pred_rs <- list.files(r_train_dir, recursive = T, full.names = T)
+    #pred_rs <- pred_rs[grepl('.tif',pred_rs)]
+    pred_rs <- all_tifs[grepl(tile_i_multiversion_for_regexpr, all_tifs)]
+    #an empty data frame to hold the data extracted for this tile
+    tile_dat <- data.frame()
 
-      #if (length(pred_rs) == 23){##for now we are working with 23 inputs per tile
-      if (length(pred_rs) == ninputs_tile){
-        #check if you have any points in this tile
-        #crop the calval to this tile
-        Pols_tile <- raster::crop(Pols, raster::raster(pred_rs[i]))
+    if (length(pred_rs) == ninputs_tile){
+      print("raster to analiza----------------------------------------------")
+      print(tile_i)
+      #reproject the trainig pols if necessary
+      if (raster::projection(Pols) != raster::projection(raster::stack(pred_rs))){
+        print("Changing projection of the shapefile")
+        Pols <- sp::spTransform(Pols, sp::CRS(raster::projection(raster::stack(pred_rs))))
+      }
 
-        #only proceed if you have training points in this tile
-        if (length(Pols_tile) > 1){
+      #check if you have any points in this tile
+      #crop the calval to this tile
+      Pols_tile <- raster::crop(Pols, raster::raster(pred_rs[i]))
+      #only proceed if you have training points in this tile
 
-          #create the brick/stack of predictor layers for this tile
-          r_train <- raster::stack(pred_rs)
-
-          cat('Sampling data from ', basename( tile_i),' which has the following layer names:\n')
-          cat(names(r_train),'\n')
-
-
-          #adjust the name so the tile-specific label is removed, and names are consistent between tiles
-          names(r_train) <- paste0('l',unlist(lapply(strsplit(names(r_train),tile_i_multiversion_for_regexpr,fixed=F),function(x){x[-1]})))
-          cat('layernames were adjust to:\n')
-          cat(names(r_train),'\n')
-
-          #reproject the trainig pols if necessary
-          if (raster::projection(Pols) != raster::projection(r_train)){
-            print("Changing projection of the shapefile")
-            Pols <- sp::spTransform(Pols, sp::CRS(raster::projection(r_train)))
-          }
-
-          #names(r_train) <- paste0('b', 1:raster::nlayers(r_train))
-
-          # extract the data for this tile for each class
-          for (i in 1:length(vuln_classes)){
-
-            pres_train <- NULL
-
-            class. <- vuln_classes[[i]]
-            cat('sampling data for class ',class.[1],'which includes ',class.[-1],'\n')
-
-            #for maxent you need presence only
-            # the sampling for polygons:
-            #Pols.train <- Pols[is.element(Pols@data[[field_name]] , class.),]
-            #take a limit set of points in the polygons to sample predictor data from
-            #pres_train <- sp::spsample(Pols.train, train_samp, type='regular', iter=25)
-            #pres_train_tile <- sp::spsample(pres_train, train_samp, type='regular', iter=25)
-
-            # the sampling for points:
-            pres_train_tile <- Pols_tile[is.element(Pols_tile@data[[field_name]] , class.),]
-
-            #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            ## you could consider taking only  a maximum set of points per tile
-            #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      print(length(Pols_tile))
+      if (length(Pols_tile) > 1){
 
 
-            #get covariates for presence locations
-            pres_dat <- data.frame(raster::extract(r_train, pres_train_tile))
+        #create the brick/stack of predictor layers for this tile
+        r_train <- raster::stack(pred_rs)
 
-            #get covariates for randomly sampled absence locations
-            abs_dat <- data.frame()
-            if (abs_samp > 0){
-              abs_loc <- dismo::randomPoints( r_train, n = abs_samp, p = pres_train_tile, warn=0 )
+        # cat('Sampling data from ', basename( tile_i),' which has the following layer names:\n')
+        # cat(names(r_train),'\n')
 
-              #exclude pseude-absences that fall too close (< 20 m) to presence locations
-              dist_abs2pres <- sp::spDists(abs_loc, sp::coordinates(Pols_tile))
-              mindist_abs2pres <- apply(dist_abs2pres, 1, min)
-              abs_loc <- abs_loc[mindist_abs2pres > 20,]
 
-              abs_dat <- data.frame(raster::extract(r_train, abs_loc))
-              abs_dat <- stats::na.omit(abs_dat)
-              if (nrow(abs_dat) == 0) {
-                stop('could not get valid background point values; is there a layer with only NA values?')
-              }
-              if (nrow(abs_dat) < abs_samp/100) {
-                stop('only got:', nrow(abs_dat), 'random background point values; is there a layer with many NA values?')
-              }
-              if (nrow(abs_dat) < abs_samp/10) {
-                warning('only got:', nrow(abs_dat), 'random background point values; Small exent? Or is there a layer with many NA values?')
-              }
-            }
+        #adjust the name so the tile-specific label is removed, and names are consistent between tiles
+        names(r_train) <- paste0('l',unlist(lapply(strsplit(names(r_train),tile_i_multiversion_for_regexpr,fixed=F),function(x){x[-1]})))
+        cat('layernames were adjust to:\n')
+        cat(names(r_train),'\n')
 
-            #join presence and absence data
-            tile_dat_class <- rbind.data.frame(pres_dat, abs_dat)
-            tile_dat_class <- cbind.data.frame(pres = c(rep(1,nrow(pres_dat)),rep(0,nrow(abs_dat))),tile_dat_class)
-            #add the classname
-            tile_dat_class$class <- rep(class., nrow(tile_dat_class) )
+        #names(r_train) <- paste0('b', 1:raster::nlayers(r_train))
 
-            #add the data for this class and tile, to the data for this tile
-            tile_dat <- rbind.data.frame(tile_dat, tile_dat_class)
+        # extract the data for this tile for each class
+        print (length(vuln_classes))
+        for (i in 1:length(vuln_classes)){
+
+          pres_train <- NULL
+
+          class. <- vuln_classes[[i]]
+          # cat('sampling data for class ',class.[1],'which includes ',class.[-1],'\n')
+
+          #for maxent you need presence only
+          # the sampling for polygons:
+          #Pols.train <- Pols[is.element(Pols@data[[field_name]] , class.),]
+          #take a limit set of points in the polygons to sample predictor data from
+          #pres_train <- sp::spsample(Pols.train, train_samp, type='regular', iter=25)
+          #pres_train_tile <- sp::spsample(pres_train, train_samp, type='regular', iter=25)
+
+          # the sampling for points:
+          pres_train_tile <- Pols_tile[is.element(Pols_tile@data[[field_name]] , class.),]
+
+          #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+          #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+          #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+          ## you could consider taking only  a maximum set of points per tile
+          #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+          #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+          #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+          #get covariates for presence locations
+          pres_dat <- data.frame(raster::extract(r_train, pres_train_tile))
+
+          #get covariates for randomly sampled absence locations
+          abs_dat <- data.frame()
+          if (abs_samp > 0){
             require(dismo)
+            abs_loc <- dismo::randomPoints( r_train, n = abs_samp, p = pres_train_tile, warn=0 )
+
+            #exclude pseude-absences that fall too close (< 20 m) to presence locations
+            dist_abs2pres <- sp::spDists(abs_loc, sp::coordinates(Pols_tile))
+            mindist_abs2pres <- apply(dist_abs2pres, 1, min)
+            abs_loc <- abs_loc[mindist_abs2pres > 20,]
+
+            abs_dat <- data.frame(raster::extract(r_train, abs_loc))
+            abs_dat <- stats::na.omit(abs_dat)
+            if (nrow(abs_dat) == 0) {
+              stop('could not get valid background point values; is there a layer with only NA values?')
+            }
+            if (nrow(abs_dat) < abs_samp/100) {
+              stop('only got:', nrow(abs_dat), 'random background point values; is there a layer with many NA values?')
+            }
+            if (nrow(abs_dat) < abs_samp/10) {
+              warning('only got:', nrow(abs_dat), 'random background point values; Small exent? Or is there a layer with many NA values?')
+            }
           }
 
+          #join presence and absence data
+          tile_dat_class <- rbind.data.frame(pres_dat, abs_dat)
+          tile_dat_class <- cbind.data.frame(pres = c(rep(1,nrow(pres_dat)),rep(0,nrow(abs_dat))),tile_dat_class)
+          #add the classname
+          tile_dat_class$class <- rep(class., nrow(tile_dat_class) )
 
-          # #join the data from this tile if to the one from the other tiles - no lo hace
-          # cat(tile_counter, ' tiles counter\n')
-          # if (tile_counter == 0){
-          #   cat('Entrooooooooo\n')
-          #   maxent_training_dfs[[class.]] <- tile_dat
-          #   print(maxent_training_dfs)
-          #   }else{
-          #     maxent_training_dfs[[class.]] <- rbind.data.frame(maxent_training_dfs[[class.]], tile_dat)
-          #     }
-          # tile_counter <- tile_counter + 1
-          # cat(tile_counter, ' tiles done, ', length(tile)-tile_counter, ' to go\n')
-        }
-        else{
-          print('There are no points falling in this tile:', tile[i],'\n')
+          #add the data for this class and tile, to the data for this tile
+          tile_dat <- rbind.data.frame(tile_dat, tile_dat_class)
+
         }
 
       }
-      #return the tile_dat at the end of each iteration
-      tile_dat
+      else{
+        print('There are no points falling in this tile:', tile[i],'\n')
+      }
+
     }
+    #return the tile_dat at the end of each iteration
+    tile_dat
+
   }
   cat("------------------------------------------\n")
 
@@ -250,11 +245,9 @@ sample_for_sicktree_model_multi_tile <- function(r_train_dir, tile = 'ALL', vuln
 
 })
   if (!is.null(data_outp_dir)){
-    #data_file <- paste0(data_outp_dir, 'maxent_training_dfs.rdata')
     data_file <- paste0(data_outp_name, '.rdsdata')
     data_file <- paste0(data_outp_dir, data_file)
-    saveRDS(tile_dat_class, file = data_file)
-    # saveRDS(maxent_training_dfs, file = data_file)
+    saveRDS(maxent_training_dfs, file = data_file)
     cat('Wrote away ', data_file,'\n')
   }
 
